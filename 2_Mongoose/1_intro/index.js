26658813d3fade92fc9d2af4820ab291b6d452d7
Mihir Gupta <mihirgupta665@gmail.com>
2025-11-21 22:40:02 +0530

    Creating first document

--- 2_Mongoose/1_intro/index.js
+++ 2_Mongoose/1_intro/index.js
@@ -18,10 +18,20 @@ main()
 const userSchema = new mongoose.Schema({    // Schema is a class whose constructor takes an object so return or forms also a object
     name: String,       // objects passed in costructor as key and pair in which key is the column name and its value is the data type
     email: String,
-    age: Number,
+    age: Number,    // mongoose adds by default a id to schema and id has the data type ObjectId
 });
 
 const User = mongoose.model("User", userSchema);
     // database name        // database name 
     // database name is converted to small leters and plural form is written.
 const Employee = mongoose.model("Employee", userSchema);
+
+const user1 = new User({name: "Mihir Gupta", email: "mihirgupta665", age: "15"});
+// the object of the model class need to be created which forms the document
+// the document object is not added just by creating it, but we could save it load it through node
+/*
+node
+.load index.js
+*/
+
+
