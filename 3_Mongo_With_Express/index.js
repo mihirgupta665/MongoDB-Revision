15cd61b61069dbdfa701432645607b612214aa9f
Mihir Gupta <mihirgupta665@gmail.com>
2025-11-23 16:02:47 +0530

    Establishing Connection with mongodb

--- 3_Mongo_With_Express/index.js
+++ 3_Mongo_With_Express/index.js
@@ -6,7 +6,14 @@ const app = express();
 app.set("views", path.join("__dirname", "views"));
 app.set("view engine", "ejs");
 
-
+async function Main(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/sigma_whatsapp");
+}
+Main().then((res)=>{
+    console.log("Connection Established");
+}).catch((err)=>{
+    console.log("Error during connetion with the database : "+err);
+})
 
 app.listen(8080, ()=>{
     console.log("Listening through port 8080...");
