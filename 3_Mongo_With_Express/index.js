a1627ad4ab98c498f6fb5388c4e9207e49ff53ba
Mihir Gupta <mihirgupta665@gmail.com>
2025-11-23 16:02:19 +0530

    Setting up home api

--- 3_Mongo_With_Express/index.js
+++ 3_Mongo_With_Express/index.js
@@ -6,6 +6,12 @@ const app = express();
 app.set("views", path.join("__dirname", "views"));
 app.set("view engine", "ejs");
 
+
+
+app.listen(8080, ()=>{
+    console.log("Listening through port 8080...");
+})
+
 app.get("/", (req, res)=>{
     res.send("Server is working fine");
 });
