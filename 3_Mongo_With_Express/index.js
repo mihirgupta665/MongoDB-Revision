c15ad580508d479333d00b640faa2490ff55d730
Mihir Gupta <mihirgupta665@gmail.com>
2025-11-24 13:46:27 +0530

    Creating a Page for edition of Sent Message

--- 3_Mongo_With_Express/index.js
+++ 3_Mongo_With_Express/index.js
@@ -74,3 +74,11 @@ app.post("/chats", (req, res)=>{
 
     res.redirect("/chats");
 })
+
+// edit message
+app.get("/chats/:id/edit", async (req,res)=>{
+    let {id} =req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", {chat} );
+    
+});
